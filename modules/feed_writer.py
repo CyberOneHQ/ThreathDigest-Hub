d6d85f478a02c212293108444f48585f0d708f37
Nicholai Imbong <51435592+CyberOneHQ@users.noreply.github.com>
2025-06-23 23:30:40 +0400

    Update feed_writer.py

--- modules/feed_writer.py
+++ modules/feed_writer.py
@@ -12,7 +12,13 @@ def generate_rss_output(entries):
         fe = fg.add_entry()
         fe.title(entry['title'])
         fe.link(href=entry['link'])
+        if 'summary' in entry:
+            fe.description(entry['summary'])
+        if 'published' in entry:
+            fe.pubDate(entry['published'])
 
-    output_file = os.path.join("data", "output", "rss_cyberattacks.xml")
+    output_path = os.path.join("data", "output")
+    os.makedirs(output_path, exist_ok=True)
+    output_file = os.path.join(output_path, "rss_cyberattacks.xml")
     fg.rss_file(output_file)
     print(f"[Output] RSS feed saved to {output_file}")
